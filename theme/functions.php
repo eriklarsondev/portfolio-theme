97a3d0f4462399fbc0154065cc3f8d442ba68ded
Erik Larson <eriklarsonco@gmail.com>
2024-09-29 00:29:43 -0400

    added custom post types

--- theme/functions.php
+++ theme/functions.php
@@ -10,3 +10,42 @@ include_once dirname(get_template_directory()) . '/classes/index.php';
  * append custom theme config below
  */
 $devfolio = new Devfolio();
+
+/******************************************************************************
+ ******************************************************************************
+ *** custom post type config
+ ******************************************************************************
+ *****************************************************************************/
+
+$devfolio->add_post_type(
+    'content block',
+    (object) [
+        'icon' => 'align left'
+    ]
+);
+
+$devfolio->add_post_type(
+    'project',
+    (object) [
+        'icon' => 'desktop',
+        'features' => ['thumbnail', 'title', 'editor', 'custom fields']
+    ]
+);
+
+$devfolio->add_post_type(
+    'tech',
+    (object) [
+        'collection' => 'Technologies',
+        'icon' => 'editor code',
+        'features' => ['title', 'custom fields']
+    ]
+);
+
+$devfolio->add_post_type(
+    'experience',
+    (object) [
+        'collection' => 'Work',
+        'icon' => 'coffee',
+        'features' => ['title', 'editor', 'custom fields']
+    ]
+);
